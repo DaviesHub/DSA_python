139be0b2045eb57d57c4729a41c450546eadff0b
DaviesHub <davieschuks.a@gmail.com>
2023-02-02 18:24:05 +0200

    Add binary search file

---
+++ binary_search.py
@@ -0,0 +1,4 @@
+def binary_search(list, target):
+    '''
+    Returns the index position of the target if found on the list, else it returns None.
+    '''
--- linear_search.py
+++ linear_search.py
@@ -1,6 +1,6 @@
 def linear_search(list, target):
     '''
-    Returns the index position of the target if found on the list, else it returns None
+    Returns the index position of the target if found on the list, else it returns None.
     '''
 
     for i in range(len(list)):
