d9aa60936c715fc20087b6d827b946ce8a0f1f94
DaviesHub <davieschuks.a@gmail.com>
2023-02-02 21:14:07 +0200

    Add binary search file

--- binary_search.py
+++ binary_search.py
@@ -1,4 +1,10 @@
 def binary_search(list, target):
     '''
-    Returns the index position of the target if found on the list, else it returns None.
+    Returns the index position of the target if found on a sorted list, else it returns None.
     '''
+
+    # Find middle of the list and compare to target position
+    if len(list) % 2 == 0:
+        i = (len(list)/2) + 1
+    else:
+        i = ((len(list)/2) + 1)/2
