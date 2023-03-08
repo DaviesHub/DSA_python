fbd5ad0f95c965ac2496cdd05caa864a27494347
DaviesHub <davieschuks.a@gmail.com>
2023-03-08 13:40:18 +0200

    Add binary search algorithm

--- binary_search.py
+++ binary_search.py
@@ -3,8 +3,29 @@ def binary_search(list, target):
     Returns the index position of the target if found on a sorted list, else it returns None.
     '''
 
-    # Find middle of the list and compare to target position
-    if len(list) % 2 == 0:
-        i = (len(list)/2) + 1
+    first = 0
+    last = len(list) - 1
+
+    while first <= last:
+        mid =  (first + last) // 2
+
+        if list[mid] == target:
+            return mid
+        elif list[mid] < target:
+            first = mid + 1
+        else:
+            last = mid - 1
+
+    return None
+
+
+def verify_search(list, target):
+    result = binary_search(list, target)
+    if result is not None:
+        return result
     else:
-        i = ((len(list)/2) + 1)/2
+        return "Not Found"
+
+nums = [2, 10, 44, 57, 62, 91, 103]
+
+print(verify_search(nums, 91))
